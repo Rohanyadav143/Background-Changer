670c638826856252e2cb25555ca87361f99b160e
Rohanyadav143 <rohn1895@gmail.com>
2026-06-08 05:56:17 +0530

    Update

--- src/Button.jsx
+++ src/Button.jsx
@@ -4,7 +4,7 @@ function Button({ color, clicked }) {
   return (
     <>
       <button className="p-1 pt-2 pb-2 outline-3 ml-15 mt-5 bg-white rounded-xl cursor-pointer" onClick={clicked}>
-        <span className="p-2 rounded-2xl" style={{ backgroundColor: color, color: textClr }}>Click Me</span>
+        <span className="p-2 rounded-2xl" style={{ backgroundColor: color, color: textClr }}>{color}</span>
       </button>
     </>
   );
